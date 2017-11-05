307c2732127cd309427072efb7ad1d2b7f996e16
ZhengYingqian <1585076433@qq.com>
2017-11-05 22:30:12 +0800

    add getRecord httpService

--- src/app/routes/survey-management/survey-management/survey-management.component.ts
+++ src/app/routes/survey-management/survey-management/survey-management.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
-import { InputcmpComponent } from '../shared/inputcmp/inputcmp.component';
+import { NzMessageService } from 'ng-zorro-antd';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import * as moment from 'moment';
+import { saveAs } from 'file-saver';
+import { _HttpClient } from '@core/services/http.client';
+import { HttpService } from '@core/services/http.service';
+import {routes} from "../../routes";
+// import { environment } from '../../../environments/environment';
+// import { InputcmpComponent } from '../shared/inputcmp/inputcmp.component';
 
 @Component({
     selector: 'app-survey-management',
@@ -26,10 +34,16 @@ export class SurveyManagementComponent implements OnInit {
 
     answers = Array();
 
-    constructor() { }
+    api = '/healthexamination/recordop/';
+    params = {"PID":"003","RecordID":"ID1"};
 
+    constructor(
+        private httpService: HttpService
+    ) { }
 
     ngOnInit() {
+        this.httpService.getRecord(this.api,this.params).subscribe((res) => {
+            console.log(res);
+        })
     }
-
 }
