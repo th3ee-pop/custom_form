6d8c63492c221d8b1ae8d3ac061dbf9157437ac0
th3eesome <610870693@qq.com>
2017-11-07 18:59:16 +0800

    create the users' introduction

--- src/app/routes/user-management/user-management.module.ts
+++ src/app/routes/user-management/user-management.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserManagementComponent } from './user-management/user-management.component';
 
-const routes:Routes = [
+const routes: Routes = [
     { path: 'detail', component: UserManagementComponent }
 ];
 
 @NgModule({
     imports: [
         CommonModule,
+        SharedModule,
         RouterModule.forChild(routes),
 
   ],
-    exports:[
+    exports: [
         RouterModule
     ],
     declarations: [
