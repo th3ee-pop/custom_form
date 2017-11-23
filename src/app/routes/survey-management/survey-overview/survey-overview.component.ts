992d15315de28baa0ee790ecd90b9b911316c188
xlbryant <xlbryant@163.com>
2017-11-23 23:14:13 +0800

    add filter

--- src/app/routes/survey-management/survey-overview/survey-overview.component.ts
+++ src/app/routes/survey-management/survey-overview/survey-overview.component.ts
@@ -29,17 +29,17 @@ export class SurveyOverviewComponent implements OnInit {
     args = {};
     _indeterminate = false;
     _allChecked = false;
-    start_time = '';
-    end_time = '';
+    start_time = ''; // 开始时间
+    end_time = '';   // 结束时间
     data = [];
     nowuser = JSON.parse(localStorage.getItem('_user'));
 
     // 所有的过滤条件在这个对象里添加
     conditions = {
-        'filter': {
-            'date_joined': []
+        'filter_dict': {
+            'Updated_time': []
         },
-        'sorted_key': 'username',
+        'sorted_key': 'PID',
         'start': (this.pi - 1) * this.ps,
         'offset': this.ps,
     };
@@ -47,12 +47,43 @@ export class SurveyOverviewComponent implements OnInit {
     ngOnInit() {
         this.getTableData();
     }
+    changePage() {
+        this.conditions.start = (this.pi - 1) * this.ps;
+        this.conditions.offset = this.ps;
+    }
 
+    showConditions() {
+        this.load();
+    }
+
+    clear() {
+        for (const key in this.conditions.filter_dict) {
+            if (this.conditions.filter_dict[key])
+                delete this.conditions.filter_dict[key];
+        }
+        this.start_time = '';
+        this.end_time = '';
+        this.load();
+    }
+    load(pi?: number) {
+        if (typeof pi !== 'undefined') {
+            this.pi = pi || 1;
+        }
+        this.setTime();
+        this.changePage();
+        this.loading = true;
+        this._allChecked = false;
+        this._indeterminate = false;
+        this.service.getPatientList(this.conditions).subscribe( (res) => {
+            this.data = res.PID_info;
+            this.loading = false;
+        });
+    }
     getTableData() {
         const conditions = {
-            'sorted_key' : 'Province',
-            'start' : '0',
-            'offset' : '20',
+            'sorted_key' : 'PID',
+            'start' : this.pi - 1,
+            'offset' : this.ps,
             'filter_dict' : { 'PID': [0, 30] }
 
         };
@@ -61,16 +92,39 @@ export class SurveyOverviewComponent implements OnInit {
         this._indeterminate = false;
         this.service.getPatientList(conditions).subscribe( (res) => {
             this.data = res.PID_info;
-            console.log(this.data);
             this.loading = false;
-           // this.total = res.Count_total;
         });
     }
+
+
+    // 时间格式转换
+    GMTToStr(time) {
+        const date = new Date(time);
+        const Str = date.getFullYear() + '-' +
+            (date.getMonth() + 1) + '-' +
+            date.getDate() + ' ' +
+            date.getHours() + ':' +
+            date.getMinutes() + ':' +
+            date.getSeconds();
+        return Str;
+    }
+
+    // 时间设置
+    setTime() {
+        if (this.start_time === '' || this.end_time === '') {
+            delete this.conditions.filter_dict.Updated_time;
+        } else {
+            this.conditions.filter_dict.Updated_time = [];
+            this.conditions.filter_dict.Updated_time.push(this.GMTToStr(this.start_time));
+            this.conditions.filter_dict.Updated_time.push(this.GMTToStr(this.end_time));
+        }
+    }
+
+
     deleteRecord(pid: string) {
         if (pid && pid !== '') {
             const deleteId = { 'PID': pid };
             this.service.deleteRecord(deleteId).subscribe((res) => {
-
                 this.getTableData();
             });
         }
