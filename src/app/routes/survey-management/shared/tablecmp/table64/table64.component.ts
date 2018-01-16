528ca50964e7ebab7065d1abcb4ce81566ece4a6
ZhengYingqian <1585076433@qq.com>
2018-01-16 10:57:34 +0800

    pass test

--- src/app/routes/survey-management/shared/tablecmp/table64/table64.component.ts
+++ src/app/routes/survey-management/shared/tablecmp/table64/table64.component.ts
@@ -18,28 +18,20 @@ export class Table64Component extends Question implements OnInit {
     rawname1 = ["胫后静脉","胫前静脉","腓静脉","腘静脉","股浅静脉","股静脉","髂静脉","髂外静脉","髂总静脉","大隐静脉"];
     rawname2 = ["肺动脉主干压力","右肺动脉","左肺动脉","肺楔压","右室压力","右房压力"];
     tf = ["否", "是"];
-    array0 = new Array(3);
+    // array0 = new Array(3);
     array1;array2;
-    array3 = new Array(6);
-    gcpa = new Array(4);
-    gcpb = new Array(4);
+    // array3 = new Array(6);
+    // gcpa = new Array(4);
+    // gcpb = new Array(4);
     jmpart = ["腰1", "腰2"];
     initialArray = new Array(55);
-    // initialArray = [
-    //     '', '', '',
-    //     '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
-    //     '', '', '', '', '',
-    //     '', '', '', '', '', '', '  ',
-    //     '', '', '', '', '', '', '',
-    //     '', '', '', '', '', '', '', '', '', '', '', ''
-    //
-    // ];
+
     idArray = [
         'gca', 'gcb', 'gcc',
         'gcd1', 'gce1', 'gcf1', 'gcg1', 'gch1', 'gci1', 'gcj1', 'gck1', 'gcl1', 'gcm1', 'gcd2', 'gce2', 'gcf2', 'gcg2',  'gch2', 'gci2', 'gcj2', 'gck2', 'gcl2', 'gcm2',
         'gcn', 'gco1', 'gco2', 'gco3', 'gco4', 'gcp',
         'gcpa1', 'gcpa2', 'gcpa3', 'gcpa4', 'gcpa5', 'gcpa6', 'gcpa7',
-        'gcpb1', 'gcpb2', 'gcpb3', 'gcpb4', 'gcpb5', 'gcpb6', 'gcpa7',
+        'gcpb1', 'gcpb2', 'gcpb3', 'gcpb4', 'gcpb5', 'gcpb6', 'gcpb7',
         'gcq1', 'gcr1', 'gcs1', 'gct1', 'gcu1', 'gcv1', 'gcq2', 'gcr2', 'gcs2', 'gct2', 'gcu2', 'gcv2'
     ];
 
@@ -50,17 +42,10 @@ export class Table64Component extends Question implements OnInit {
       for (let i = 0; i < this.array1.length; i++) { this.array1[i] = new Array(this.rawname1.length); }
       this.array2 = new Array(2);
       for (let i = 0; i < this.array2.length; i++) { this.array2[i] = new Array(this.rawname2.length); }
-      // this.array0 = this.initialArray.slice(0,3);
-      // this.array1[0] = this.initialArray.slice(3,13);
-      // this.array1[1] = this.initialArray.slice(13,23);
-      // this.array3 = this.initialArray.slice(23,29);
-      // // console.log(this.array1);
-      // this.answerChange();
   }
 
     getAnswer() {
         this.changedAnswer = [];
-        // this.getInitArray();
         this.setAnswer();
         console.log(this.changedAnswer);
     }
@@ -68,33 +53,24 @@ export class Table64Component extends Question implements OnInit {
     setAnswer() {
       for (let i = 0; i < this.initialArray.length; i++) {
             if (this.initialArray[i] && this.initialArray[i] !== '') {
+                if(i == 0 || i==2 || (27<i&&i<43)){
+                    console.log(i);
+                    console.log( this.question.id2[i]);
+                    console.log( this.initialArray[i]);
                 this.changedAnswer.push({
                     Record_ID: this.idArray[i],
-                    Record_Value: this.initialArray[i]
+                    Record_Value: this.initialArray[i]+1
                 });
+            }else {
+                    this.changedAnswer.push({
+                        Record_ID: this.idArray[i],
+                        Record_Value: this.initialArray[i]
+                    });
+                }
             }
         }
     }
 
-    getJmparts(gcpArray){
-      let temp = new Array(3);
-      let gcp;
-      if(gcpArray[0] ===2 ){
-          gcp = [gcpArray[0],...temp,gcpArray[1],gcpArray[2],gcpArray[3]];
-      }else{
-          gcp = [...gcpArray,...temp];
-      }
-      return gcp;
-    }
-
-    getInitArray(){
-        let  gcp = new Array(14);
-        if( this.array3[5] === 1){
-            gcp = [...this.getJmparts(this.gcpa),
-                ...this.getJmparts(this.gcpb)];
-        }
-        this.initialArray = [...this.array0,...this.array1[0] ,...this.array1[1],...this.array3,...gcp,...this.array2[0],...this.array2[1]];
-    }
     answerChange(){
 
     }
