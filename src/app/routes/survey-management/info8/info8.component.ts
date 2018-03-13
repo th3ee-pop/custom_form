ce654ad3f4df9c8f8c08103d9a3621480e722c0a
th3eesome <610870693@qq.com>
2018-03-13 20:19:32 +0800

    part8

--- src/app/routes/survey-management/info8/info8.component.ts
+++ src/app/routes/survey-management/info8/info8.component.ts
@@ -21,13 +21,9 @@ export class Info8Component extends Question implements OnInit {
 
     answerSet = {
         Id: '',
+        Time: 0,
         answers: []
     }
-    startStr = 'ea';
-    column = 20;
-    row = 6;
-    letterArray = [];
-    idArray = [];
     rowTitle_1 = [ '走行(1:正常,2:异常)', '内径(1:正常,2:变宽,3:变窄)', '宫腔(1:清晰,2:不清晰，可见暗淡回声填充,3:完全闭塞)', '静脉压缩性(1:好,2:差)', '侧枝(1:可见侧枝形成,2:无侧枝形成)', '静脉瓣功能(1:良好,2:关闭不全)'];
     columnTitle_1 = ['髂外静脉', '股总静脉', '股浅静脉', '股深静脉', '腘静脉', '胫后静脉', '胫前静脉', '大隐静脉', '腓静脉', '肌间静脉'];
     secondLevelTitle_1 = ['左', '右', '左', '右', '左', '右', '左', '右', '左', '右', '左', '右', '左', '右', '左', '右', '左', '右', '左', '右'];
@@ -51,6 +47,12 @@ export class Info8Component extends Question implements OnInit {
         '', '',
         '', ''
     ];
+    rowTitle_4 = [''];
+    columnTitle_4 = ['主动脉', '升主', '主动脉弓', '降主', '腹主', '肺动脉', '室间隔厚度',
+        '室间隔搏幅', '左室后壁厚度', '左室后壁搏幅', '左心房1', '左心房2', '右心房',
+        '右心室1', '右心室2', '右室流出道', '左心室1', '左心室2', '(mm1)', '(mm2)',
+        '左右室壁运动减弱', '室间隔运动减低', '肺动脉收缩压_三尖瓣反流法估测', 'EF', 'FS',
+        '总体评价1', '总体评价2', '彩色血流示1', '彩色血流示2'];
 
     rowTitle_5 = ['CK', 'CKMB(u/L)', 'CtnT(ng/ml)', 'MYO(ng/ml)'];
     columnTitle_5 = ['入院时', '第二次检查'];
@@ -92,7 +94,7 @@ export class Info8Component extends Question implements OnInit {
     }
 
     ngOnInit() {
-        for (let i = 0; i < 11; i++) {
+        for (let i = 0; i < 6; i++) {
             this.tabs.push({
                 name   : `出院/术后 ${i}`,
                 content: `Content of tab ${i}`
@@ -112,9 +114,66 @@ export class Info8Component extends Question implements OnInit {
             }
             });
         console.log(this.rowTitle_10);
+        console.log(this.rowTitle_2.slice(0, 8));
+
+
+        const answer_table1 = this.initAnswerBox('下肢静脉超声', this.rowTitle_1, this.columnTitle_1, ['左', '右'], [1, 4]);
+        const answer_table2 = this.initAnswerBox( '心电图', this.rowTitle_2.slice(0, 8), this.columnTitle_2, null, [3, 4]);
+        const answer_table3 = this.initAnswerBox('其他检查', this.rowTitle_3, this.columnTitle_3);
+        const answer_table4 = this.initAnswerBox('心脏超声', this.rowTitle_4, this.columnTitle_4);
+        const answer_table5 = this.initAnswerBox('心肌酶谱', this.rowTitle_5, ['是否'].concat(this.columnTitle_5));
+        const answer_table6 = {answerId: '6分钟步行试验', answerValue: ''};
+        const answer_table7 = this.initAnswerBox('血常规', this.rowTitle_6, this.columnTitle_6);
+        const answer_table8 = this.initAnswerBox('凝血项目', this.rowTitle_7, this.columnTitle_7);
+        const answer_table9 = this.initAnswerBox('VTE危险因素', this.rowTitle_8, ['是否'].concat(this.columnTitle_8));
+        const answer_table10 = this.initAnswerBox('C反应蛋白', this.rowTitle_9, ['是否'].concat(this.columnTitle_9));
     }
 
-    initAnswerBox(row: number, column: number, id: string) {
-        
+    initAnswerBox(table: string,
+                  row: Array<string>,
+                  column: Array<string>,
+                  secondLevel_column ?: Array<string> ,
+                  extraAnswer ?: Array<number>) {
+        const answerBox = {
+            table_name: table,
+            answer_array: []
+        };
+        let new_column;
+        if (secondLevel_column) {
+            const temp_column = [];
+            for (let i = 0; i < column.length; i++) {
+                for (let j = 0; j < secondLevel_column.length; j++) {
+                    temp_column.push(column[i] + secondLevel_column[j]);
+                }
+            }
+            new_column = temp_column;
+        } else {
+            new_column = column;
+        }
+        for (let line = 0; line < row.length; line ++) {
+            answerBox.answer_array.push([]);
+            for (let vertical = 0; vertical < new_column.length; vertical ++) {
+                const answer = {
+                    answerId: table + '_' + row[line] + '_' + new_column[vertical],
+                    answerValue: ''
+                };
+                answerBox.answer_array[line].push(answer);
+            }
+        }
+        if (extraAnswer) {
+            console.log(extraAnswer);
+            for (let i = 0; i < extraAnswer[0]; i ++) {
+                answerBox.answer_array.push([]);
+                for (let j = 0; j < extraAnswer[1]; j++) {
+                    answerBox.answer_array[answerBox.answer_array.length - 1].push({
+                        answerId: table + '_总体评价' + i + j,
+                        answerValue: ''
+                    });
+                }
+            }
+
+        }
+        console.log(answerBox);
+        return answerBox;
     }
 }
